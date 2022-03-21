47cef37f9fa05770208d0c4b10e081afb5e4a791
Seph Freer <sephfreer@gmail.com>
2022-03-21 22:31:46 +1100

    Polished, for Heroku. Swe4r, seems inaccurate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,13 @@ class PagesController < ApplicationController
   def about
   end
   def home
-    # render plain: 'Hello World!'
-    # @page = Page.new
-    # @date_search = params[:q] ? params[:q] : "1984/05/10"
-    @date_search = params[:birth] && params[:birth] != "" ? params[:birth] : 
-      params[:q] && params[:q] != "" ? params[:q].gsub('/','-') : "1980-09-22T23:54"
-    @date_search += "T12:00" if !@date_search[/T/]
+    @date_search = params[:birth_time] && params[:birth_time] != "" ? params[:birth_time] : "1980-09-22T23:54"
     @rising = params[:rising] ? params[:rising] : "Unknown"
+    @commit = params[:commit] ? true : false
     @time_zone = params[:time_zone] ? params[:time_zone] : "Sydney"
     utc_offset = @date_search.to_datetime.in_time_zone(@time_zone).strftime('%z')
-    @utc = eval(utc_offset[0]+utc_offset[1..2]+'+'+(utc_offset[3..4].to_f/60).to_s)
+    @utc = eval(utc_offset[0]+utc_offset[1..2]+'+'+(utc_offset[3..4].to_f/60).to_s) # Float from +0730 string format -> 7.5
+    @julday = helpers.julday(@date_search)
+    p params.to_enum.to_h, @rising, @julday # For debugging
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,6 +8,7 @@ module PagesHelper
     (ra/30).to_i
   end
   def blurb(sun,moon,mercury,venus,mars,ascnum)
+    # Inputs are the right-ascension values of each of the named planetary bodies
     blurb = ['I am ','',' but my emotions are rather ','','. I think in a ','',' way, but express my energy in a ','',' way. In love, I seek ','','. I take on the role of ','','.']
     # out = ["Sun in "+ra2sign(sun)+", Moon in "+ra2sign(moon)+", Mercury in "+ra2sign(mercury)+", Venus in "+ra2sign(venus)+" and Mars in "+ra2sign(mars)]
     plist = [ra2num(sun),ra2num(moon),ra2num(mercury),ra2num(mars),ra2num(venus),ascnum]
@@ -16,7 +17,7 @@ module PagesHelper
     ['direct','practical','lighthearted','intuitive','dynamic','fruitful','artistical','complex','curious','purposeful','logical','vague'],
     ['combative','abiding','erratic','protective','powerful','efficient','passive','brooding','lively','focused','analytical','creative'],
     ['passion','commitment','stimulation','security','affection','respect','support','devotion','excitement','intensity','understanding','stability'],
-    ['the pioneer','the owner','the messenger','the nurturer','the protector','the organizer','the aesthete','the mystic','the comedian','the old soul','the outlaw','the creator','the {ASCENDANT UNKNOWN ERROR}']]
+    ['the pioneer','the owner','the messenger','the nurturer','the protector','the organizer','the aesthete','the mystic','the comedian','the old soul','the outlaw','the creator','the {...}']]
     (0..5).each do |i|
       planet = plist[i]
       blurb[i*2+1] = blbans[i][planet]
@@ -35,6 +36,35 @@ module PagesHelper
   end
   def ymdhm(date)
     # Split a date into an array of year, month, day, hour and minute - for creating Time
-    
+    # Abandoned for now, lol
+  end
+  def swe_name(planet)
+    planet = planet.upcase.tr(' ','_')  # e.g. SUN, VESTA, MEAN_NODE, TRUE_NODE, etc.
+    if %w[ECL_NUT SUN MOON MERCURY VENUS MARS JUPITER SATURN URANUS NEPTUNE PLUTO MEAN_NODE TRUE_NODE MEAN_APOG OSCU_APOG EARTH CHIRON PHOLUS CERES PALLAS JUNO VESTA INTP_APOG INTP_PERG NPLANETS FICT_OFFSET NFICT_ELEM PLMOON_OFFSET AST_OFFSET CUPIDO HADES ZEUS KRONOS APOLLON ADMETOS VULKANUS POSEIDON ISIS NIBIRU HARRINGTON NEPTUNE_LEVERRIER NEPTUNE_ADAMS PLUTO_LOWELL PLUTO_PICKERING].include?(planet)
+      planet = eval("Swe4r::SE_"+planet)
+    else
+      "fail"
+    end
+    planet
+  end
+  def swe_calc_ut(julian_day, planet)
+    Swe4r::swe_calc_ut(julian_day, swe_name(planet), Swe4r::SEFLG_SWIEPH|Swe4r::SEFLG_SPEED|Swe4r::SEFLG_EQUATORIAL)#|Swe4r::SEFLG_TOPOCTR - Swe4r::SEFLG_MOSEPH instead of SEFLG_SWIEPH
+  end
+  def julday(date)
+    y, m, d = date.split(/[-\/]/)
+    y, m = y.to_i, m.to_i
+    if d[/T/]
+      # yyyy-mm-ddTHH:MM format assumed
+      h = d[/\d\d(?=:)/].to_i + d[/(?<=:)\d\d/].to_f/60
+      d = d.to_i
+    else  # No time formatting included, could be integer day or float (e.g. 1981-04-12.234)
+      h = (d.to_f % 1) * 24
+    end
+    d -= 1 if h < @utc
+    h -= @utc   # Adjust for UTC offset in timezone, make the time UT.
+    Swe4r::swe_julday(y, m, d.to_i, h)
+  end
+  def swe_houses(julian_day, lat=-33.8, long=151.2099, hsys='Placidus'[0].ord)
+    Swe4r::swe_houses(julian_day, lat, long, hsys)
   end
 end
