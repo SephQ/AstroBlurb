32d89cee5720d7a38f26ed881b1317194e92084c
Seph Freer <sephfreer@gmail.com>
2022-03-22 12:36:49 +1100

    Polished, for Heroku. Swe4r 0.0.2, seems inaccurate

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module AstroBlurb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Zeitwerk auto-loader is having issues on Heroku: `on_file_autoloaded': expected file /app/app/models/pages.rb to define constant Pages
+    # Swapping to classic for now:
+    config.autoloader = :classic
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
