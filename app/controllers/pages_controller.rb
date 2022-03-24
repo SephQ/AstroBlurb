90b012e2b1a893db04303733f73bf0e786b86516
Seph Freer <sephfreer@gmail.com>
2022-03-24 21:54:34 +1100

    Fix utc time zone issue.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
     @commit = params[:commit] ? true : false
     @time_zone = params[:time_zone] ? params[:time_zone] : "Sydney"
     utc_offset = @date_search.to_datetime.in_time_zone(@time_zone).strftime('%z')
-    @utc = eval(utc_offset[0]+utc_offset[1..2]+'+'+(utc_offset[3..4].to_f/60).to_s) # Float from +0730 string format -> 7.5
+    @utc = eval(utc_offset[0]+utc_offset[1..2].to_i.to_s+utc_offset[0]+(utc_offset[3..4].to_f/60).to_s) # Float from +0730 string format -> 7.5
     @long = @time_zone == "Sydney" ? 151.2099 : @utc * 15   # Rough guess at longitude based on timezone
     @lat = -33.8651   # Sydney assumed for now, hard to solve this from timezone.
     @julday = helpers.julday(@date_search)
