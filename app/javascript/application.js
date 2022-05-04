b07b86b5a7a5179e935a915a664dac5280d361e0
SephQ <solfreer@gmail.com>
2022-05-04 19:07:57 +1000

    Reinstated application.js but left application.coffee out since it's not supported since Rails 6, Social Share Buttons test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@
 // 220424 https://dev.to/coorasse/rails-7-bootstrap-5-and-importmaps-without-nodejs-4g8
 import "popper"
 import "bootstrap"
+
+// https://stackoverflow.com/questions/55046244/problem-with-the-social-share-button-gem-for-rails
+//= require social-share-button
